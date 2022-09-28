61f42ef8446fcaabf32cac8dbda3bfff0495ed25
thescientist230803 <thescientistian@gmail.com>
2022-09-28 18:36:49 -0400

    dddd

--- 0x04-python-more_data_structures/3-common_elements.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
-=======
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
->>>>>>> c1c5f9e6cde8cfee0f0e8fc4ec33c45207945bcc
